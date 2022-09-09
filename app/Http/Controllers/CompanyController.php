49239447188e87e7a91e601d5ea7a74c5cf0c885
send_80085 <frrhazizov@gmail.com>
2022-09-09 10:17:55 +0300

    Get/Post/Patch/Delete first version

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -4,20 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Company;
+use Floor9design\JsonApiFormatter\Models\JsonApiFormatter;
+use Floor9design\JsonApiFormatter\Models\DataResource;
+
+
 
 class CompanyController extends Controller
 {
-    public function GetAllCompany()
+     public function GetAllCompany()
     {
+        $json_api_formatter = new JsonApiFormatter();
         $company = Company::all();
-        return response()->json($company);
+        $response = $json_api_formatter->dataResourceResponseArray($company);
 
     }
 
     public function GetCompanyId($id)
     {
+        /*$company = Company::find($id);
+        return response()->json($company);*/
+
         $company = Company::find($id);
-        return response()->json($company);
+        $company = Parser::parseRequestString($company);
     }
 
 }
